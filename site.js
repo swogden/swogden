26aa09d28b262bb6e4b5916a78dbd235656b7146
Stephen Ogden <swogden2.0@gmail.com>
2026-08-23 18:39:03 -0400

    Add files via upload

--- site.js
+++ site.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
+  document.querySelectorAll('.fb-share-popup').forEach(function(link){
+    link.addEventListener('click', function(e){
+      e.preventDefault();
+      var url = link.getAttribute('href');
+      var width = 626, height = 436;
+      var left = (window.screen.width / 2) - (width / 2);
+      var top = (window.screen.height / 2) - (height / 2);
+      window.open(
+        url,
+        'facebook-share-dialog',
+        'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top + ',menubar=no,toolbar=no,location=no,status=no'
+      );
+    });
+  });
+
   document.querySelectorAll('.copy-link-btn').forEach(function(btn){
     btn.addEventListener('click', function(){
       var url = btn.getAttribute('data-url');
